f82bdd9c259ddca416e54f8de695bde7020f99a2
yu <2758345412@qq.com>
2023-11-30 19:51:06 +0800

    LeetCode hot 100

--- src/main/java/leetCode/hot100/hot100.java
+++ src/main/java/leetCode/hot100/hot100.java
@@ -211,7 +211,7 @@ public class hot100 {
         for (int end = 0; end < s.length(); end++) {
             char ch = s.charAt(end);
             if (map.containsKey(ch)) {
-                start = Math.max(start, map.get(ch)+1);
+                start = Math.max(start, map.get(ch) + 1);
             }
             map.put(ch, end);
             res = Math.max(res, end - start + 1);
@@ -222,34 +222,34 @@ public class hot100 {
 
     /**
      * 438 找到字符串中所有字母异位符
+     *
      * @param s
      * @param p
      * @return
      */
-    public  List<Integer> findAnagrams(String s, String p) {
+    public List<Integer> findAnagrams(String s, String p) {
         ArrayList<Integer> list = new ArrayList<>();
-        int len=p.length();
-        for (int i = 0; i <= s.length()-len; i++) {
-            String s1=s.substring(i,i+len);
-            if(s1.equals(p)){
+        int len = p.length();
+        for (int i = 0; i <= s.length() - len; i++) {
+            String s1 = s.substring(i, i + len);
+            if (s1.equals(p)) {
                 list.add(i);
-            }
-            else if (isAnagrams(s1,p)){
+            } else if (isAnagrams(s1, p)) {
                 list.add(i);
             }
         }
         return list;
     }
 
-    public boolean isAnagrams(String a,String b){
+    public boolean isAnagrams(String a, String b) {
         int[] arr = new int[26];
 
         for (int i = 0; i < a.length(); i++) {
-            arr[a.charAt(i)-'a']++;
+            arr[a.charAt(i) - 'a']++;
         }
 
         for (int i = 0; i < b.length(); i++) {
-            if (--arr[b.charAt(i)-'a']<0){
+            if (--arr[b.charAt(i) - 'a'] < 0) {
                 return false;
             }
         }
@@ -258,28 +258,29 @@ public class hot100 {
 
     /**
      * 560 和为K的子数组
+     *
      * @param nums
      * @param k
      * @return
      */
     public int subarraySum(int[] nums, int k) {
-        int count=0;
+        int count = 0;
         int[] arr = new int[nums.length];
 
-        arr[0]=nums[0];
+        arr[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            arr[i]=arr[i-1]+nums[i];
+            arr[i] = arr[i - 1] + nums[i];
         }
 
-        int l=0,r=nums.length;
+        int l = 0, r = nums.length;
 
         for (int i = 0; i < nums.length; i++) {
-            if (arr[i]==k){
+            if (arr[i] == k) {
                 count++;
             }
 
-            for (int j = i+1; j < nums.length; j++) {
-                if (arr[j]-arr[i]==k){
+            for (int j = i + 1; j < nums.length; j++) {
+                if (arr[j] - arr[i] == k) {
                     count++;
                 }
             }
@@ -290,18 +291,228 @@ public class hot100 {
 
     /**
      * 53 最大子数组和
+     *
      * @param nums
      * @return
      */
     public int maxSubArray(int[] nums) {
-        int pre=0,max=nums[0];
+        int pre = 0, max = nums[0];
 
         for (int i = 0; i < nums.length; i++) {
-            pre = Math.max(pre+nums[i],nums[i]);
-            max=Math.max(max,pre);
+            pre = Math.max(pre + nums[i], nums[i]);
+            max = Math.max(max, pre);
         }
 
         return max;
+    }
+
+    /**
+     * 56 合并区间
+     *
+     * @param intervals
+     * @return
+     */
+    public int[][] merge(int[][] intervals) {
+        //先对各数组的第一位进行排序
+        Arrays.sort(intervals, (v1, v2) -> v1[0] - v2[0]);
+
+        int[][] res = new int[intervals.length][2];
+        int index = -1;
+
+        for (int[] interval : intervals) {
+            //如果是第一位就直接放入到res中
+            //如果是res[index][1]大于interval[0],也直接放进去
+            //例如，res=[[1,2]],interval=[3,4],不在res[index]区间范围内,=> res=[[1,2],[3,4]]
+            if (index == -1 || interval[0] > res[index][1]) {
+                res[++index] = interval;
+            } else {
+                //分两种情况，一种是包含，一种是有相交区间,取两个数组的第二位最大值
+                //包含:res=[[1,10]],interval=[2,3],=>res=[[1,10]]
+                //另一种是相交res=[[1,3]],interval=[2,5],=>res=[1,5]
+                res[index][1] = Math.max(res[index][1], interval[1]);
+            }
+        }
+
+        return Arrays.copyOf(res, index + 1);
+    }
+
+    /**
+     * 189 轮转数组
+     *
+     * @param nums
+     * @param k
+     */
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] newArr = new int[n];
+        for (int i = 0; i < n; i++) {
+            newArr[i] = nums[(i + k) % n];
+        }
+        System.arraycopy(newArr, 0, nums, 0, n);
+    }
+
+
+    /**
+     * 238 除以自身以外数组的乘积
+     *
+     * @param nums
+     * @return
+     */
+    public int[] productExceptSelf(int[] nums) {
+        int[] L = new int[nums.length];
+        int[] R = new int[nums.length];
+        int[] res = new int[nums.length];
+
+        //左累乘，左边没有元素，故为1
+        L[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            L[i] = L[i - 1] * nums[i - 1];
+        }
+
+        R[nums.length - 1] = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            R[i] = R[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = L[i] * R[i];
+        }
+
+        return res;
+    }
+
+
+    /**
+     * 73 矩阵置零
+     *
+     * @param matrix
+     */
+    public void setZeroes(int[][] matrix) {
+        //行
+        int[] row = new int[matrix.length];
+        //列
+        int[] col = new int[matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = col[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+    }
+
+
+    /**
+     * 160 相交链表
+     *
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        int lenA = 0, lenB = 0;
+
+        while (curA != null) {
+            lenA++;
+            curA = curA.next;
+        }
+
+        while (curB != null) {
+            lenB++;
+            curB = curB.next;
+        }
+
+        int len = Math.abs(lenA - lenB);
+
+        //curA是最长那个
+        curA = lenA - lenB >= 0 ? headA : headB;
+        curB = lenA - lenB < 0 ? headA : headB;
+
+        while (len > 0) {
+            curA = curA.next;
+            len--;
+        }
+
+        while (curA != null) {
+            if (curA == curB) {
+                return curA;
+            }
+            curA = curA.next;
+            curB = curB.next;
+        }
+
+        return null;
+    }
+
+
+    /**
+     * 反转链表
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = null;
+        ListNode cur = head;
+        ListNode res = null;
+
+        while (cur != null) {
+            temp = cur.next;
+            cur.next = res;
+            res = cur;
+            cur = temp;
+        }
+
+        return res;
+    }
+
+
+    /**
+     * 234 回文链表
+     *
+     * @param head
+     * @return
+     */
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode pre = null;
+        ListNode temp = null;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+
+            //进行反转
+            temp = slow.next;
+            slow.next = pre;
+            pre = slow;
+            slow = temp;
+        }
+
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        while (pre != null) {
+            if (pre.val != slow.val) {
+                return false;
+            }
+            slow = slow.next;
+            pre = pre.next;
+        }
+        return true;
     }
 
 
--- src/main/java/leetCode/questionOfDay/Main.java
+++ src/main/java/leetCode/questionOfDay/Main.java
@@ -1,5 +1,7 @@
 package leetCode.questionOfDay;
 
+import org.junit.Test;
+
 import java.util.*;
 
 public class Main {
@@ -35,5 +37,43 @@ public class Main {
         }
 
         return count;
+    }
+
+    /**
+     * 1657 确定两个字符串是否接近
+     *
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public boolean closeStrings(String word1, String word2) {
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+
+        for (int i = 0; i < word1.length(); i++) {
+            char ch = word1.charAt(i);
+            arr1[ch-'a']++;
+        }
+
+        for (int i = 0; i < word2.length(); i++) {
+            char ch = word2.charAt(i);
+            arr2[ch-'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if ((arr1[i] == 0 && arr2[i] > 0) || (arr1[i] > 0 && arr2[i] == 0)) {
+                return false;
+            }
+        }
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1,arr2);
+    }
+
+    @Test
+    public void test() {
+        closeStrings("abc", "bca");
     }
 }
